d1e9f00dcfd7f1c5e7c23b60d7e8521a7227c6f0
ImCemix <72702597+ImCemix@users.noreply.github.com>
2020-10-11 18:28:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ client.remove_command('help')
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="1.0v"))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="2.0v"))
     client.reaction_roles = []
 
 @client.command(aliases=["Clear"])
